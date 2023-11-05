03132308d5192dbbab72c7d55f03924e8a904c40
kevgilmore <kgilmore.me@gmail.com>
2023-11-05 11:29:43 +0000

    icon bug

--- src/App.js
+++ src/App.js
@@ -124,7 +124,7 @@ const App = () => {
         setTimePassed(0)
 
         setIsPaused(true)
-        setPausePlayIcon(getIcon())
+        setPausePlayIcon(<CaretRightOutlined/>)
         clearInterval(intervalRef.current);
     }
 
