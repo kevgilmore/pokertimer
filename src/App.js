fe72f2e43366cbf4e3883bb58aa5e47c8f1432c8
kevgilmore <kgilmore.me@gmail.com>
2025-05-16 19:15:20 +0100

    debug emailer log lines2

--- src/App.js
+++ src/App.js
@@ -48,6 +48,7 @@ const App = () => {
 
     const handleSubmit = (values) => {
         console.log('Service ID again:', process.env.REACT_APP_EMAILJS_SERVICE_ID);
+        console.log('Template ID again:', process.env.REACT_APP_EMAILJS_TEMPLATE_ID);
         emailjs.send(
             process.env.REACT_APP_EMAILJS_SERVICE_ID,
             process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
