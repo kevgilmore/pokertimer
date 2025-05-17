72aafa7e853ae4d2a43c1d96565d865df6890873
kevgilmore <kgilmore.me@gmail.com>
2025-05-17 16:09:36 +0100

    add pageview ga

--- src/App.js
+++ src/App.js
@@ -77,12 +77,13 @@ const App = () => {
     let intervalRef = useRef();
 
     useEffect(() => {
+        ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
         localStorage.setItem('game', JSON.stringify(game));
          if(!isPaused) {
             intervalRef.current = setInterval(updateTimer, ONE_SECOND);
         }
         return () => clearInterval(intervalRef.current);
-
     });
 
     const startGame = () => {
